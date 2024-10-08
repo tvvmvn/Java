0898df5bc6f781d9e7a07f91a8c99e4954061610
tvvmvn <tvvmvn@gmail.com>
2024-10-08 14:49:19 +0900

    10.8

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Java
 
 ### Basic
-1. Intro
-1. Data Type
+1. Variables
 1. String
 1. Array
 1. Operator
-1. Conditional Statements
-1. Loop
+1. if statements
+1. switch statements
+1. for Loop
+1. while Loop
+1. Modifiers
 1. Error handling
 
 ### Class
 1. Instance
 1. Methods
 1. Constructor
+1. ..
---
+++ access-modifier.java
@@ -0,0 +1,13 @@
+public class Main {
+  private String fname = "John";
+  private String lname = "Doe";
+  private String email = "john@doe.com";
+  private int age = 24;
+  
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Email: " + myObj.email);
+    System.out.println("Age: " + myObj.age);
+  }
+}
